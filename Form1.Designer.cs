b025fe07c760399006a83592b5596b25e58c9b2c
iputak <43246823+iputak@users.noreply.github.com>
2019-09-11 17:39:36 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace DOTN_projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.id = new System.Windows.Forms.Label();
+            this.unesi = new System.Windows.Forms.Button();
+            this.ime = new System.Windows.Forms.Label();
+            this.upis = new System.Windows.Forms.Label();
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.prezime = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.azuriraj = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.izbrisi = new System.Windows.Forms.Button();
+            this.topPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // id
+            // 
+            this.id.AutoSize = true;
+            this.id.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.id.Location = new System.Drawing.Point(35, 122);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(120, 20);
+            this.id.TabIndex = 0;
+            this.id.Text = "Broj iskaznice";
+            // 
+            // unesi
+            // 
+            this.unesi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unesi.Location = new System.Drawing.Point(157, 258);
+            this.unesi.Name = "unesi";
+            this.unesi.Size = new System.Drawing.Size(57, 30);
+            this.unesi.TabIndex = 1;
+            this.unesi.Text = "Unesi";
+            this.unesi.UseVisualStyleBackColor = true;
+            this.unesi.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ime
+            // 
+            this.ime.AutoSize = true;
+            this.ime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ime.Location = new System.Drawing.Point(34, 159);
+            this.ime.Name = "ime";
+            this.ime.Size = new System.Drawing.Size(39, 20);
+            this.ime.TabIndex = 2;
+            this.ime.Text = "Ime";
+            // 
+            // upis
+            // 
+            this.upis.AutoSize = true;
+            this.upis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.upis.Location = new System.Drawing.Point(34, 230);
+            this.upis.Name = "upis";
+            this.upis.Size = new System.Drawing.Size(96, 20);
+            this.upis.TabIndex = 3;
+            this.upis.Text = "Zadnji upis";
+            this.upis.Click += new System.EventHandler(this.godine_Click);
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.topPanel.Controls.Add(this.pictureBox1);
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(595, 92);
+            this.topPanel.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::DOTN_projekt.Properties.Resources.logo2;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(595, 92);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(157, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 24);
+            this.textBox1.TabIndex = 5;
+            // 
+            // prezime
+            // 
+            this.prezime.AutoSize = true;
+            this.prezime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prezime.Location = new System.Drawing.Point(34, 194);
+            this.prezime.Name = "prezime";
+            this.prezime.Size = new System.Drawing.Size(73, 20);
+            this.prezime.TabIndex = 6;
+            this.prezime.Text = "Prezime";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(157, 159);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 24);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(157, 194);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(212, 24);
+            this.textBox3.TabIndex = 8;
+            // 
+            // azuriraj
+            // 
+            this.azuriraj.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.azuriraj.Location = new System.Drawing.Point(220, 258);
+            this.azuriraj.Name = "azuriraj";
+            this.azuriraj.Size = new System.Drawing.Size(73, 30);
+            this.azuriraj.TabIndex = 10;
+            this.azuriraj.Text = "Ažuriraj";
+            this.azuriraj.UseVisualStyleBackColor = true;
+            this.azuriraj.Click += new System.EventHandler(this.azuriraj_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 318);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(523, 150);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy.MM.dd";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(157, 229);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(212, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox2.Location = new System.Drawing.Point(411, 119);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(134, 130);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // izbrisi
+            // 
+            this.izbrisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.izbrisi.Location = new System.Drawing.Point(299, 258);
+            this.izbrisi.Name = "izbrisi";
+            this.izbrisi.Size = new System.Drawing.Size(70, 30);
+            this.izbrisi.TabIndex = 14;
+            this.izbrisi.Text = "Izbriši";
+            this.izbrisi.UseVisualStyleBackColor = true;
+            this.izbrisi.Click += new System.EventHandler(this.izbrisi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 512);
+            this.Controls.Add(this.izbrisi);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.azuriraj);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.prezime);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.topPanel);
+            this.Controls.Add(this.upis);
+            this.Controls.Add(this.ime);
+            this.Controls.Add(this.unesi);
+            this.Controls.Add(this.id);
+            this.Name = "Form1";
+            this.Text = "Strongdor";
+            this.topPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label id;
+        private System.Windows.Forms.Button unesi;
+        private System.Windows.Forms.Label ime;
+        private System.Windows.Forms.Label upis;
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label prezime;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button azuriraj;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button izbrisi;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DOTN_projekt
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            prikaziPodatke();
+        }
+        
+        Image Slika;
+        string displayimg, filePath;
+        string folderpath = @"C:\\Dotn";
+        //Unos podataka
+        private void button1_Click(object sender, EventArgs e)
+        {
+                
+                string imeSlike = textBox1.Text + ".jpg";
+                string mapa = "C:\\Dotn";
+                string pathstring = System.IO.Path.Combine(mapa, imeSlike); 
+
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Users VALUES (@BrojIskaznice,@Ime,@Prezime,@ZadnjiUpis, @Slika)", con);
+                cmd.Parameters.AddWithValue("@BrojIskaznice", int.Parse(textBox1.Text));
+                cmd.Parameters.AddWithValue("@Ime", textBox2.Text);
+                cmd.Parameters.AddWithValue("@Prezime", textBox3.Text);
+                cmd.Parameters.AddWithValue("@ZadnjiUpis", dateTimePicker1.Text);
+                Slika.Save(pathstring);
+                cmd.Parameters.AddWithValue("@Slika", pathstring);
+               // Slika.Save(pathstring);
+            
+                //Slika.Copy(filePath, pathstring, true);
+                //cmd.Parameters.AddWithValue("@Slika", textBox2.Text);
+
+            cmd.ExecuteNonQuery();
+
+                con.Close();
+                MessageBox.Show("Uspjesno spremljeno!");
+                
+                prikaziPodatke();
+                makniPodatke();
+        }
+        
+
+        //Ažuriranje korisnika
+        private void azuriraj_Click(object sender, EventArgs e)
+        {
+            string imeSlike = textBox1.Text + ".jpg";
+            string mapa = "C:\\Dotn";
+            string pathstring = System.IO.Path.Combine(mapa, imeSlike);
+
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Users SET Ime=@Ime,Prezime=@Prezime,ZadnjiUpis=@ZadnjiUpis,Slika=@Slika WHERE BrojIskaznice=@BrojIskaznice", con);
+                cmd.Parameters.AddWithValue("@BrojIskaznice", int.Parse(textBox1.Text));
+                cmd.Parameters.AddWithValue("@Ime", textBox2.Text);
+                cmd.Parameters.AddWithValue("@Prezime", textBox3.Text);
+                cmd.Parameters.AddWithValue("@ZadnjiUpis", dateTimePicker1.Text);
+                Slika.Save(pathstring);
+                cmd.Parameters.AddWithValue("@Slika", pathstring);
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+                MessageBox.Show("Uspjesno spremljeno!");
+
+                prikaziPodatke();
+                makniPodatke();
+            }
+            else
+            {
+                MessageBox.Show("Označite korisnika za ažuriranje");
+            }
+        }
+        //Prikaz podataka
+        private void prikaziPodatke()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Users", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        //Micanje podataka iz forme za unos  
+        private void makniPodatke()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        
+        //Brisanje korisnika
+        private void izbrisi_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DELETE Users WHERE BrojIskaznice=@BrojIskaznice", con);
+
+                cmd.Parameters.AddWithValue("@BrojIskaznice", int.Parse(textBox1.Text));
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Korisnik je izbrisan!");
+                prikaziPodatke();
+                makniPodatke();
+            }
+            else
+            {
+                MessageBox.Show("Označite korisnika za brisanje");
+            }
+        }
+
+        //Prikaz odabranog iz tablice
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            dateTimePicker1.Value = Convert.ToDateTime(dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString());
+            Slika = Image.FromFile(dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString());
+            pictureBox2.Image = Slika;
+        }
+        //Odabir slike
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = "Odaberite sliku";
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp;)|*.jpg; *.jpeg; *.gif; *.bmp;";
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                
+                Slika = Image.FromFile(open.FileName);
+                pictureBox2.Image = Slika;
+                // Putanja slike
+                filePath = open.FileName;
+            }
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void godine_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
